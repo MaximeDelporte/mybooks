2bf7f111950876364a6375f65b4adef89a3c15dc
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-02-17 12:02:13 +0100

    Project initialisation

---
+++ mybooks/mybooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  mybooksApp.swift
+//  mybooks
+//
+//  Created by afx on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct mybooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
