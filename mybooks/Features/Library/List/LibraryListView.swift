86d3cde5677d54ef5ff686b7fefe16242397897a
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-02-18 09:00:24 +0100

    Setup LibraryListView empty mode.

--- mybooks/Features/Library/List/LibraryListView.swift
+++ mybooks/Features/Library/List/LibraryListView.swift
@@ -13,6 +13,30 @@ struct LibraryListView: View {
     @Binding var path: NavigationPath
     
     var body: some View {
-        Text("LibraryListView")
+        VStack {
+            Image("library-home")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Text("Start your digital library")
+                .font(.system(size: 22, weight: .bold, design: .rounded))
+                .foregroundStyle(.primary)
+                .padding(.vertical)
+            
+            Text("Add your first book to get started.")
+                .font(.system(size: 16, weight: .regular, design: .rounded))
+                .foregroundStyle(.primary)
+            
+            MBButton(title: "Add Your First Book", style: .secondary, action: {})
+                .padding(.top)
+            
+            Spacer()
+        }
+        .padding(.horizontal, 22)
     }
+}
+
+#Preview {
+    @State var path: NavigationPath = .init()
+    return LibraryListView(path: $path)
 }
