0fb69377e87e2f04eda669d5091d2d947813baa2
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-02-20 23:06:47 +0100

    Remove mocks from models. Add userId property inside Book model.

--- mybooks/Common/Models/User.swift
+++ mybooks/Common/Models/User.swift
@@ -12,9 +12,4 @@ struct User: Identifiable, Codable {
     let firstName: String
     let lastName: String
     let email: String
-    let books: [Book]
-}
-
-extension User {
-    static var MOCK_USER = User(id: "123", firstName: "Maxime", lastName: "Delporte", email: "email@email.com", books: Book.MOCK_BOOKS)
 }
